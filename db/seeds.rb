3f83d5e844b3a4b882918144f7601d5dad1c0850
hylobates-lar <alison.v.quaglia@gmail.com>
2020-01-14 18:47:48 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Adam", last_name: "Sandler")
+Student.create(first_name: "Chris", last_name: "Farley")
+Student.create(first_name: "David", last_name: "Spade")
+
+SchoolClass.create(title: "Chlorophyll, more like Borophyll", room_number: 14)
+SchoolClass.create(title: "Spanish Armada", room_number: 64)
+SchoolClass.create(title: "One Piece of Ace", room_number: 3)
